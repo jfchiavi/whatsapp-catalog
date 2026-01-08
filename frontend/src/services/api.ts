3fd9aebd0090d6f63d0d510c0750d0f4a71096c0
jfchiavi <eljavi379@gmail.com>
2026-01-07 22:15:26 -0300

    add credentials for token by cookie, it would be usefull later

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,6 +3,7 @@ import { useAuthStore } from '@/store/auth.store';
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, // IMPORTANTE para refresh token por cookie
 });
 /*
  El interceptor:
